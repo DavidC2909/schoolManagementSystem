dca858081c42b64731834db086cfa2278c7ebe6a
David <davidcostea09@gmail.com>
2023-08-04 00:13:47 +0300

    Added student class.

---
+++ src/school/management/system/Student.java
@@ -0,0 +1,55 @@
+package school.management.system;
+
+
+// This class is responsible for keeping track of students (names, grades, fees).
+public class Student {
+
+    private int id;
+    private String name;
+    private double grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    //The role of the constructor is to create a new object(student) by initializing.
+
+    /**
+     *
+     * Fees for every student is going to be $30,000.
+     * Fees paid initially is $0.
+     * @param id for the student: unique.
+     * @param name
+     * @param grade
+     */
+    public Student(int id, String name, double grade){
+        feesPaid=0;
+        feesTotal=30000;
+
+        this.id=id;
+        this.name=name;
+        this.grade=grade;
+
+    }
+
+    //Not going to alter student's names, student's id.
+
+    /**
+     * Used to update the student's grade.
+     * @param grade is the new grade of the student.
+     */
+    public void setGrade(int grade){
+        this.grade=grade;
+    }
+
+    /**
+     * Keep adding the fees to feesPaid Field.
+     * Add the fees to the fees paid.
+     * The school is going to receive the funds.
+     *
+     * @param fees the fees that the student pays.
+     */
+    public void updateFeesPaid(int fees){
+        feesPaid=feesPaid+fees;
+    }
+
+
+}
